b69141f74fbdab57592f0e4edb32c3b783bdfb62
mojca <mstampar@asu.edu>
2019-10-07 15:42:18 -0700

    Final version of AddingMachine.java and the accompanying JUnit test named AddingMacj=hineTest.java

--- assign2/src/cse360assign2/AddingMachine.java
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -1,51 +1,87 @@
 /**
- * The AddingMachine program for Assignment2
+ * The AddingMachine class for Assignment2
  * 
  * @author mojca stampar
- * @version 1.0
+ * @version 2.0 final
  * @since 2019-06-10
  */
 package cse360assign2;
+import java.lang.*;
 
 public class AddingMachine {
 
+	/**
+	 * The total of our calculations and the history of our transactions
+	 */
 	private int total;
+	private String history;
 	
-	public AddingMachine () {
+	/**
+	 * Creates a new AddingMachine object with 0 total and history 
+	 * containing just the 0 total
+	 */
+	public AddingMachine () 
+	{
 		total = 0;  // not needed - included for clarity
-	}
-	
-	public int getTotal () {
-		return 0;
+		history = Integer.toString(total);
 	}
 	
 	/**
-	 * This method is used to
+	 * A method returning a current total from AddingMachine
+	 * @return current total
+	 */
+	public int getTotal () 
+	{
+		//System.out.println(Integer.toString(total));
+		return total;
+	}
+	
+	/**
+	 * This method is used to add the parameter to the total variable
+	 ** and update the String containing the history
 	 * @param value This is the parameter for add method
 	 * @return Nothing
 	 */
-	public void add (int value) {
-		
+	public void add (int value)
+	{
+		total = total + value;
+		//System.out.println(Integer.toString(total));
+		history = history + "+" + Integer.toString(value);
+		//System.out.println(history);
+		return;
 	}
 	
 	/**
-	 * This method is used to
+	 * This method is used to subtract the parameter from the total value
+	 * and update the String containing the history
 	 * @param value This is the parameter for subtract method
 	 * @return Nothing
 	 */
-	public void subtract (int value) {
-		
+	public void subtract (int value) 
+	{
+		total = total - value;
+		//System.out.println(Integer.toString(total));
+		history = history + "-" + Integer.toString(value);
+		//System.out.println(history);
+		return;
 	}
 	
 	/**
-	 * This method is used to print out
+	 * This method is used to return the history of transactions
 	 * @return String This method is returning a string
 	 */
-	public String toString () {
-		return "";
+	public String toString ()
+	{
+		//System.out.println(history);
+		return history;
 	}
 
-	public void clear() {
-	
+	/**
+	 * This method clears the history and resets the total to 0
+	 */
+	public void clear()
+	{
+		total = 0;  // not needed - included for clarity
+		history = Integer.toString(total);
 	}
 }
--- assign2/src/cse360assign2/AddingMachineTest.java
+++ assign2/src/cse360assign2/AddingMachineTest.java
@@ -3,37 +3,75 @@ package cse360assign2;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.JUnitCore; 
+import org.junit.runner.Result; 
+import org.junit.runner.notification.Failure; 
 
-public class AddingMachineTest
-{
+public class AddingMachineTest {
+
 
 	@Test
 	public void testGetTotal() 
 	{
-		AddingMachine myCalculator = new AddingMachine;
+		AddingMachine myCalculator = new AddingMachine();
 		int expectedTotal = 0;
 		int actualTotal = myCalculator.getTotal();
 		assertEquals(expectedTotal,actualTotal);
 	}
 
 	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	public void testAdd() 
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		int expectedTotal = 4;
+		myCalculator.add(4);
+		int actualTotal = myCalculator.getTotal();
+		assertEquals(expectedTotal,actualTotal);
 	}
 
 	@Test
-	public void testSubtract() {
-		fail("Not yet implemented");
+	public void testSubtract()
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		int expectedTotal = -4;
+		myCalculator.subtract(4);
+		int actualTotal = myCalculator.getTotal();
+		assertEquals(expectedTotal,actualTotal);
 	}
 
 	@Test
-	public void testToString() {
-		fail("Not yet implemented");
+	public void testToString() 
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		String expectedHistory = Integer.toString(0);
+		String actualHistory = myCalculator.toString();
+		assertEquals(expectedHistory,actualHistory);
+	}
+	
+	@Test
+	public void testHistoryUpdates() 
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add(4);
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		String expectedHistory = "0+4-2+5";
+		String actualHistory = myCalculator.toString();
+		assertEquals(expectedHistory,actualHistory);
 	}
 
 	@Test
-	public void testClear() {
-		fail("Not yet implemented");
+	public void testClear() 
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		myCalculator.add(4);
+		myCalculator.clear();
+		String expectedHistory = Integer.toString(0);
+		String actualHistory = myCalculator.toString();
+		assertEquals(expectedHistory,actualHistory);
+		int expectedTotal = 0;
+		int actualTotal = myCalculator.getTotal();
+		assertEquals(expectedTotal,actualTotal);
 	}
 
 }
