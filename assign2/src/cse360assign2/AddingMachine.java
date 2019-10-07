84814aadcaa27ee6583f8503730b39ce37c7597e
mojca <mstampar@asu.edu>
2019-10-07 14:15:17 -0700

    Initial addingMachine

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,51 @@
+/**
+ * The AddingMachine program for Assignment2
+ * 
+ * @author mojca stampar
+ * @version 1.0
+ * @since 2019-06-10
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * This method is used to
+	 * @param value This is the parameter for add method
+	 * @return Nothing
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This method is used to
+	 * @param value This is the parameter for subtract method
+	 * @return Nothing
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * This method is used to print out
+	 * @return String This method is returning a string
+	 */
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
